4b1adfe7293f03bea2d5871777ee87f339ef1267
JoanEbreso <joannaebreso@gmail.com>
2025-01-07 18:02:50 +0000

    project-setup

--- src/main/java/com/myschool/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.myschool.repository;
-
-import com.myschool.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
