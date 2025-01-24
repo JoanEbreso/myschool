2c132164595fae03008f5ebbf211aa80756b6306
JoanEbreso <joannaebreso@gmail.com>
2025-01-24 09:51:22 +0000

    security filter

--- src/main/java/com/myschool/controller/AppUserController.java
+++ src/main/java/com/myschool/controller/AppUserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "api/vi/user_registration")
+@RequestMapping(path = "api/v1/user_registration")
 public class AppUserController {
 
     @Autowired
--- src/main/java/com/myschool/security/config/WebSecurityConfig.java
+++ src/main/java/com/myschool/security/config/WebSecurityConfig.java
@@ -1,12 +1,26 @@
 package com.myschool.security.config;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
 public class WebSecurityConfig{
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception{
+        http
+                .csrf().disable()
+                .authorizeRequests().requestMatchers("/api/v*/user_registration/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated().and()
+                .formLogin();
+    }
 
 }
